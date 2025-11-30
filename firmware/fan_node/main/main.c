b0b1b9b5ad6b9ce0332ca98c7da4ae1e11e41777
Jincheol Park <67998022+Clearsu@users.noreply.github.com>
2025-11-30 16:28:18 +0900

    feat: dht22 sensor firmware (#8)

--- firmware/fan_node/main/main.c
+++ firmware/fan_node/main/main.c
@@ -14,19 +14,20 @@ static system_state_t g_state;
 
 void sensor_task(void *arg)
 {
-	float t;
+	float temperature;
+	float humidity;
 
-	t = 24.0f;
+	vTaskDelay(pdMS_TO_TICKS(2000));
+	dht_gpio_init();
 	while (1)
 	{
-		t += 0.5f;
-		if (t > 32.0f)
+		if (dht_read(&temperature, &humidity))
 		{
-			t = 24.0f;
-		}
-		g_state.temperature = t;
-		ESP_LOGI("SENSOR", "fake temp = %.1f", t);
-		vTaskDelay(pdMS_TO_TICKS(2000));
+			g_state.temperature = temperature;
+			ESP_LOGI("SENSOR", "temp = %.1f, humidity = %.1f", temperature, humidity);
+		} else
+			ESP_LOGE("SENSOR", "sensor error");
+		vTaskDelay(pdMS_TO_TICKS(2100));
 	}
 }
 
@@ -37,36 +38,26 @@ void fan_control_task(void *arg)
 	while (1)
 	{
 		if (g_state.temperature >= THRESHOLD)
-		{
 			g_state.fan_on = true;
-		}
 		else
-		{
 			g_state.fan_on = false;
-		}
 		ESP_LOGI("CTRL", "temp = %.1f fan = %s", g_state.temperature, g_state.fan_on ? "ON" : "OFF");
-		vTaskDelay(pdMS_TO_TICKS(500));
+		vTaskDelay(pdMS_TO_TICKS(2000));
 	}
 }
 
 void app_main(void)
 {
-	/*
 	BaseType_t ret;
 
 	ESP_LOGI("MAIN", "app_main start");
 	ret = xTaskCreate(sensor_task, "sensor_task", 4096, NULL, 5, NULL);
 	if (ret != pdPASS)
-	{
 		ESP_LOGE("MAIN", "Failed to create sensor_task");
-	}
 	ret = xTaskCreate(fan_control_task, "fan_control_task", 4096, NULL, 5, NULL);
 	if (ret != pdPASS)
-	{
 		ESP_LOGE("MAIN", "Failed to create sensor_task");
-	}
 	ESP_LOGI("MAIN", "app_main end");
-	*/
 
 	/*
 	uint8_t buf[64];
@@ -82,12 +73,5 @@ void app_main(void)
 	printf("\n");
 	*/
 
-	float temp = 0;
-	float humidity = 0;
 
-	esp_rom_delay_us(3000000);
-
-	dht_gpio_init();
-	dht_read(&temp, &humidity);
-	printf("temp: %f, humidity: %f\n", temp, humidity);
 }
