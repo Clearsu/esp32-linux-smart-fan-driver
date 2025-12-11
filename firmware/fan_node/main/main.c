9cf3090b365b9e12c36581a69e5f57a140b784c0
Clearsu <clearwater1219@gmail.com>
2025-12-11 23:47:16 +0900

    chore: remove comment

--- firmware/fan_node/main/main.c
+++ firmware/fan_node/main/main.c
@@ -31,18 +31,4 @@ void app_main(void)
 		ESP_LOGE("MAIN", "Failed to create cmd_handler_task");
 
 	ESP_LOGI("MAIN", "app_main end");
-
-	/*
-	uint8_t buf[64];
-	uint16_t out_len;
-
-	uint8_t payload[] = {0x12, 0x84};
-
-	proto_build_frame(0x81, 0x01, payload, sizeof(payload), buf, &out_len);
-	printf("TX frame (%d bytes): ", out_len);
-	for (int i = 0; i < out_len; i++) {
-		printf("%02X ", buf[i]);
-	}
-	printf("\n");
-	*/
 }
