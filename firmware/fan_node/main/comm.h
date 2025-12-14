9f7369843b9f7f2ee9784ef7eb7aa90d914ef822
Clearsu <clearwater1219@gmail.com>
2025-12-14 16:44:04 +0900

    feat: add UART1 for data channel

--- firmware/fan_node/main/comm.h
+++ firmware/fan_node/main/comm.h
@@ -3,7 +3,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "driver/uart.h"
+
 #include "proto.h"
+
+#define COMM_UART UART_NUM_1
+#define COMM_TX_PIN 16
+#define COMM_RX_PIN 17
 
 void	comm_init(void);
 bool	comm_send_frame(const proto_frame_t *frame);
